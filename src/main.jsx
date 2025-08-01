25429b34d50f5bda2fff830b4d1715f7938ffc5f
kangyi <1729166956@qq.com>
2025-08-01 15:50:18 +0800

    完成移动端适配

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'lib-flexible'  // 移动端适配
 import App from './App.jsx'
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Router>
+    <App />
+  </Router>
 )
